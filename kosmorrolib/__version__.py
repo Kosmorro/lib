f5580351af4080961e102f7b5e9f95deff8352e0
Jérôme Deuchnord <jerome@deuchnord.fr>
2021-04-05 12:47:11 +0200

    build: bump version to 0.10.0

--- kosmorrolib/__version__.py
+++ kosmorrolib/__version__.py
@@ -3,7 +3,7 @@
 __title__ = "kosmorrolib"
 __description__ = "A library that computes your ephemerides"
 __url__ = "http://kosmorro.space"
-__version__ = "0.9.0"
+__version__ = "0.10.0"
 __build__ = 0x000900
 __author__ = "Jérôme Deuchnord"
 __author_email__ = "jerome@deuchnord.fr"
